4a8ea4b5410b564b5ef469bd527e2e05952d55c1
huiyaoren <wslshanlin@sina.com>
2016-12-20 16:27:00 +0800

    添加命令码处理

--- test/server.py
+++ test/server.py
@@ -10,12 +10,20 @@ if os.name != 'nt': # 不是 NT 系统的话使用 epoll
     from twisted.internet import epollreactor
     epollreactor.install()
 
+def command_1(_conn, data):
+    print('233~')
+    print(data)
+
 if __name__ == '__main__':
     # 日志输出
     log.startLogging(sys.stdout)
 
     # 服务 服务端数据的逻辑处理
-    service = services.Service("testService")
+    # service = services.Service("testService")
+    service = services.CommandService("testService")
+
+    # 添加命令码处理
+    service.mapTarget(command_1)
 
     # 工厂类 用于处理数据封装 协议头封装 分包 粘包的处理
     factory = LiberateFactory()
